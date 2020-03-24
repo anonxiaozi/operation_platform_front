feae48a48380e334ec088a3ff53d40bd43bce8bc
wangshiyuan <bloke_anon@126.com>
2020-03-24 17:45:31 +0800

    xterm attach

--- src/components/MyTerminal.vue
+++ src/components/MyTerminal.vue
@@ -10,33 +10,11 @@ import { FitAddon } from 'xterm-addon-fit';
 import { Base64 } from "js-base64";
 import { WebLinksAddon } from "xterm-addon-web-links"
 import { SearchAddon } from "xterm-addon-search"
+import { AttachAddon } from 'xterm-addon-attach';
 
-var bindTerminal = (term, websocket) => {
-    term.write('\r\nWelcome to Felix Web Ssh Terminal\n\r\n');
-    // send heartbeat package to avoid closing webSocket connection in some proxy environmental such as nginx.
-    var send_data = ''
-    var heartBeatTimer = setInterval(function() {
-        websocket.send(JSON.stringify({ type: "heartbeat", message: "" }));
-    }, 20 * 1000);
-    websocket.addEventListener("open", () => {
-        term.onData(data => {
-            send_data += data
-            websocket.send(
-                JSON.stringify({
-                    message: Base64.encode(send_data)
-                })
-            )
-        })
-    });
-    websocket.addEventListener("message", (event) => {
-        term.writeln(event.data);
-    })
-    websocket.addEventListener("close", function() {
-        console.log('close.')
-    });
-};
 export default {
     mounted() {
+        var send_data = ""
         const host_addr = this.$route.params.host;
         const term = new Terminal({
             termName: host_addr
@@ -49,11 +27,40 @@ export default {
         term.loadAddon(searchAddon);
         term.open(this.$refs.terminal);
         term.write('Hello....[' + host_addr + ']');
-        const webSocket = new WebSocket('ws://10.15.101.58:80/t/');
+        const webSocket = new WebSocket('ws://10.15.101.58:80/ws/chat/first/');
+        webSocket.onopen = () => {
+            console.log('ws onopen')
+        }
+        webSocket.onmessage = event => {
+            term.writeln(event.data)
+            console.log(
+                // Base64.decode(JSON.parse(event.data))
+                event.data
+            )
+        }
         webSocket.onclose = ce => {
             console.log(ce)
         };
-        // bindTerminal(term, webSocket, true, 0);
+        const attachAddon = new AttachAddon(socket);
+        term.loadAddon(attachAddon);
+        // term.attachCustomKeyEventHandler(function(e) {
+        //     if (e.keyCode == 13) {
+        //         console.log('enter')
+        //         webSocket.send(
+        //             JSON.stringify({
+        //                 message: Base64.encode(send_data)
+        //             })
+        //         )
+        //         send_data = ''
+        //     }
+        // });
+        // term.onData(data => {
+        //     if (data !== "") {
+        //         term.write(data)
+        //         send_data += data
+        //     }
+        // });
+        // // bindTerminal(term, webSocket, true, 0);
     },
     props: ['dialogTerminalVisible'],
     methods: {
